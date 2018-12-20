7f12736d422ae21391de78446d0981d84fcbef05
Selim <selimlebbady@gmail.com>
2018-12-20 09:50:48 +0100

    logo displaying everywhere

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+
 class Home extends Component {
   render() {
     return (
       <div className="landing-page">
-        <img className="landing-page-image" src="images/logo.svg" alt="logo ih"/>
+        <img className="landing-page-image" src={logo} alt="logo ih"/>
         
       </div>
     );
